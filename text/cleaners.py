d14afe1bf038cb1f0308e3daa0767e974f3a1627
TimilsinaBimal <timilsina.bml05@gmail.com>
2022-05-27 13:18:54 +0545

    Add function Nepali Number to Text

--- text/cleaners.py
+++ text/cleaners.py
@@ -1,90 +1,66 @@
 """ from https://github.com/keithito/tacotron """
 
 '''
-Cleaners are transformations that run over the input text at both training and eval time.
+Cleaners are transformations that run over the input text at both training and
+eval time.
+import matplotlib
+%matplotlib inline
+import matplotlib.pylab as plt
 
-Cleaners can be selected by passing a comma-delimited list of cleaner names as the "cleaners"
+import IPython.display as ipd
+
+import sys
+Cleaners can be selected by passing a comma-delimited list of cleaner names
+as the "cleaners"
 hyperparameter. Some cleaners are English-specific. You'll typically want to use:
   1. "english_cleaners" for English text
-  2. "transliteration_cleaners" for non-English text that can be transliterated to ASCII using
+  2. "transliteration_cleaners" for non-English text that can be transliterated
+  to ASCII using
      the Unidecode library (https://pypi.python.org/pypi/Unidecode)
-  3. "basic_cleaners" if you do not want to transliterate (in this case, you should also update
+  3. "basic_cleaners" if you do not want to transliterate (in this case, you
+  should also update
      the symbols in symbols.py to match your data).
 '''
 
 import re
 from unidecode import unidecode
-from .numbers import normalize_numbers
 
+from .numbers import normalize_numbers
+from .numToWords import NumberToNepaliText
 
 # Regular expression matching whitespace:
 _whitespace_re = re.compile(r'\s+')
 
-# List of (regular expression, replacement) pairs for abbreviations:
-_abbreviations = [(re.compile('\\b%s\\.' % x[0], re.IGNORECASE), x[1]) for x in [
-  ('mrs', 'misess'),
-  ('mr', 'mister'),
-  ('dr', 'doctor'),
-  ('st', 'saint'),
-  ('co', 'company'),
-  ('jr', 'junior'),
-  ('maj', 'major'),
-  ('gen', 'general'),
-  ('drs', 'doctors'),
-  ('rev', 'reverend'),
-  ('lt', 'lieutenant'),
-  ('hon', 'honorable'),
-  ('sgt', 'sergeant'),
-  ('capt', 'captain'),
-  ('esq', 'esquire'),
-  ('ltd', 'limited'),
-  ('col', 'colonel'),
-  ('ft', 'fort'),
-]]
-
-
-def expand_abbreviations(text):
-  for regex, replacement in _abbreviations:
-    text = re.sub(regex, replacement, text)
-  return text
-
-
-def expand_numbers(text):
-  return normalize_numbers(text)
-
-
-def lowercase(text):
-  return text.lower()
-
 
 def collapse_whitespace(text):
-  return re.sub(_whitespace_re, ' ', text)
+    """Removes whitespaces"""
+    return re.sub(_whitespace_re, ' ', text)
 
 
 def convert_to_ascii(text):
-  return unidecode(text)
+    return unidecode(text)
+
+
+def convert_sentence_number_to_text(sentence):
+    """Convert numbers into numerical number. like for '१' it will
+    transform this to एक"""
+    ntnt = NumberToNepaliText()
+    numbers = ['१', '२', '३', '४', '५', '६', '७', '८', '९', '०']
+    tokens = sentence.split(" ")
+    new_tokens = []
+    for token in tokens:
+        for number in numbers:
+            if number in token:
+                new_tokens.append(ntnt.NumberToText(token))
+                break
+        else:
+            new_tokens.append(token)
+    return " ".join(new_tokens)
 
 
 def basic_cleaners(text):
-  '''Basic pipeline that lowercases and collapses whitespace without transliteration.'''
-  text = lowercase(text)
-  text = collapse_whitespace(text)
-  return text
-
-
-def transliteration_cleaners(text):
-  '''Pipeline for non-English text that transliterates to ASCII.'''
-  text = convert_to_ascii(text)
-  text = lowercase(text)
-  text = collapse_whitespace(text)
-  return text
-
-
-def english_cleaners(text):
-  '''Pipeline for English text, including number and abbreviation expansion.'''
-  text = convert_to_ascii(text)
-  text = lowercase(text)
-  text = expand_numbers(text)
-  text = expand_abbreviations(text)
-  text = collapse_whitespace(text)
-  return text
+    '''Basic pipeline that lowercases and collapses whitespace without
+    transliteration.'''
+    text = collapse_whitespace(text)
+    text = convert_sentence_number_to_text(text)
+    return text
--- text/symbols.py
+++ text/symbols.py
@@ -6,13 +6,21 @@ Defines the set of symbols used in text input to the model.
 The default is a set of ASCII characters that works well for English or text that has been run through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details. '''
 from text import cmudict
 
-_pad        = '_'
-_punctuation = '!\'(),.:;? '
+_pad = '_'
+_punctuation = '|!\'(),.:;? '
 _special = '-'
-_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+# _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+_letters = [
+    'क', 'ख', 'ग', 'घ', 'ङ', 'च', 'छ', 'ज', 'झ', 'ञ', 'ट', 'ठ', 'ड', 'ढ', 'ण',
+    'त', 'थ', 'द', 'ध', 'न', 'ऩ', 'प', 'फ', 'ब', 'भ', 'म', 'य', 'र', 'ल', 'व',
+    'श', 'ॠ', 'ष', 'ष', 'स', 'ह', 'क्ष', 'त्र', 'ज्ञ', "ँ", '।', 'ा', 'ू', 'ु',
+    'े', 'ो', 'ौ', 'ै', 'ृ', 'ी', 'ि', 'ं', 'ॐ', 'ः', '?', '!', '.', 'ळ', 'ण',
+    ',', 'अ', 'आ', 'इ', 'ई', 'ऊ', 'उ', 'ए', 'ऐ', 'ओ', 'औ', 'ञ', ' ', '्',
+    '\u200d', 'ऋ', '\u200c']
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
 _arpabet = ['@' + s for s in cmudict.valid_symbols]
 
 # Export all symbols:
-symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
+symbols = [
+    _pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
